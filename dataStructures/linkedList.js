7eea4dca342118b1ddc4919577bd3f059c0ca3f3
Nahuel Uboldi <nahueluboldi@gmail.com>
2023-04-30 22:00:44 -0300

    linked list data structure added

---
+++ dataStructures/linkedList.js
@@ -0,0 +1,62 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  // add a new element last
+  append(val) {
+    // if ther is no elements
+    if (this.head === null) {
+      this.head = new Node(val);
+      return;
+    }
+
+    // generic addition to the list
+    let curr = this.head;
+    while (curr.next !== null) {
+      curr = curr.next;
+    }
+    curr.next = new Node(val);
+  }
+
+  // print elements of the list
+  print() {
+    let str = '';
+    let curr = this.head;
+    while (curr !== null) {
+      str += curr.val + ' -> ';
+      curr = curr.next;
+    }
+    console.log(str);
+  }
+
+  // check if list contains a certain value
+  contains(target) {
+    let curr = this.head;
+    while (curr !== null) {
+      if (curr.val === target) {
+        return true;
+      }
+      curr = curr.next;
+    }
+    return false;
+  }
+}
+
+const list = new LinkedList();
+list.append('a');
+list.append('b');
+list.append('c');
+list.append('d');
+list.append('e');
+list.print();
+
+console.log(list.contains('a')); // true
+console.log(list.contains('z')); // false
